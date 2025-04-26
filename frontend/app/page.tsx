d074b7c24650ce9c01bb3a85caecaff5492a7786
Nibir Islam <100664014+nibir036@users.noreply.github.com>
2025-04-26 12:13:00 +0600

    Update page.tsx

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -8,11 +8,11 @@ import CreateProjectPage from "./projects/create-project";
 export default function Home() {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
-     <LandingPage/>
+     {/* <LandingPage/> */}
      {/* <CreateProjectPage/> */}
      {/* <LogIn/> */}
      {/* <SignUp/> */}
-     {/* <Hpage/> */}
+     <Hpage/>
      {/* <AdminLogIn/> */}
      {/* <Dashboard/> */}
     </main>
