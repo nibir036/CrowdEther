fc7b2af23bffc5011837550c3f051dfe1a11bd17
Muktadirul Islam Nibir <muktadirul.islam.nibir@g.bracu.ac.bd>
2026-07-01 18:04:04 +0600

    added favicon

--- .next/server/webpack-runtime.js
+++ .next/server/webpack-runtime.js
@@ -125,7 +125,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("cadf2993aa643168")
+/******/ 		__webpack_require__.h = () => ("bb180070d5b22cd1")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
--- app/layout.tsx
+++ app/layout.tsx
@@ -10,6 +10,10 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="shortcut icon" type="image/x-icon" href="/favicon.ico" />
+        <link rel="icon" type="image/x-icon" href="/favicon.ico" />
+      </head>
       <body>
         <GradientBackground />
         {children}
